a855130eb871f463fc54ec3b0601d8a4a4ce119f
alexander <ahansen1335@gmail.com>
2023-12-03 08:34:00 +0100

    update schema

--- schema.sql
+++ schema.sql
@@ -1,2 +1,3 @@
 CREATE TABLE internships (internship_ID INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, business_name TEXT NOT NULL);
+CREATE TABLE sqlite_sequence(name,seq);
 CREATE TABLE students (student_ID INTEGER PRIMARY KEY UNIQUE NOT NULL, last_name TEXT NOT NULL, first_name TEXT NOT NULL, internship_choice NUMERIC REFERENCES internship (internship_ID));
