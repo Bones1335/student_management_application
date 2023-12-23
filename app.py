c041beaeab51966b4ee25956ca8e4fddeb0ea9db
alexander <ahansen1335@gmail.com>
2023-12-23 10:47:19 +0100

    remove unnecessary comment from app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ def get_db_connection():
     connection = sqlite3.connect("students.db", check_same_thread=False)
     connection.row_factory = sqlite3.Row
     return connection
-    # db_cursor = db_connect.cursor()
 
 @app.route("/")
 def home():
