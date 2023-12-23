265173129e8264dfaa90524680aa9146a26861b9
alexander <ahansen1335@gmail.com>
2023-12-23 08:10:11 +0100

    create home.html template for the home page with corresponding route in app.py

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def home():
+    return render_template('home.html')
+
+@app.route("/enroll")
+def enroll_student():
     return render_template('create_new_student.html')
