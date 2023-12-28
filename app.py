f1ae645b23f6f9ca5c7092bf87e5459723b253c0
alexander <ahansen1335@gmail.com>
2023-12-28 09:13:31 +0100

    implement create new student with randomly generated id and an empty internship choice string for the MVP

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
+from modules import random_student_id as gen_id
 
 app = Flask(__name__)
 
@@ -13,24 +14,33 @@ def get_db_connection():
 def home():
     return render_template('home.html')
 
-@app.route("/enroll")
-def enroll_student():
-    return render_template('create_new_student.html')
-
-@app.route("/students")
+@app.route("/students", methods=["GET", "POST"])
 def show_students():
     connection = get_db_connection()
     students = connection.execute("SELECT * FROM students").fetchall()
     return render_template('students.html', students=students)
 
-@app.route("/internships")
+@app.route("/internships", methods=["GET", "POST"])
 def show_internships():
     connection = get_db_connection()
     internships = connection.execute("SELECT * FROM internships").fetchall()
     return render_template('internships.html', internships=internships)
 
-@app.route("/create_new_student")
+@app.route("/create_new_student", methods=["GET", "POST"])
 def create_new_student():
+    return render_template('create_new_student.html')
+
+@app.route("/enroll", methods=["POST"])
+def enroll_student():
     connection = get_db_connection()
-    internships = connection.execute("SELECT * FROM students").fetchall()
-    return render_template('create_new_student.html', internships=internships)
+
+    student_ID = gen_id.n_len_rand(7)
+    last_name = request.form.get("last_name")
+    first_name = request.form.get("first_name")
+    internship_choice = ''
+
+    connection.execute("INSERT INTO students VALUES(?, ?, ?, ?)", (student_ID, last_name, first_name, internship_choice))
+    connection.commit()
+
+    return redirect('/students')
+    
