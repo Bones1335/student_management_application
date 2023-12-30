9c5c75c3fcf19c7bc95e18f61c6ccedff0a6148c
Scott Hansen <tech@firecat53.net>
2023-12-30 14:24:41 -0800

    Create database and tables on initial startup

--- modules/db_connection.py
+++ modules/db_connection.py
@@ -1,6 +1,12 @@
 import sqlite3
 
-def get_db_connection():
+def get_db_connection(app):
     connection = sqlite3.connect("students.db", check_same_thread=False)
+    create_tables(connection, app)
     connection.row_factory = sqlite3.Row
     return connection
+
+def create_tables(connection, app):
+    with connection:
+        with app.open_resource('schema.sql', mode='r') as f:
+            connection.executescript(f.read())
